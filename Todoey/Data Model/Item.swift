a540fcce0e4e0445f45898da7eda9194cc958015
Jameson Cheo <jamesoncheo@jamesons-mac.local>
2018-03-10 23:42:38 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jameson Cheo on 2018-03-12.
+//  Copyright © 2018 Jameson Cheong. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
